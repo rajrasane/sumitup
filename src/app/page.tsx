7fb66cb34518443e4c1fffe3bf445f6c8cbe06f5
Raj Rasane <rasaneraj04@gmail.com>
2025-04-12 10:50:15 +0530

    did a lot of changes to optimize overall website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
   return (
     <div className="relative w-full">
       <SmoothScroll />
-      <BgGradient />
+      {/* <BgGradient /> */}
       <div className="flex flex-col ">
-        <HeroSection />
+        <HeroSection /> 
         <DemoSection />
         <HowItWorksSection />
         <PricingSection />
--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 export default function HeroSection() {
   return (
-    <section className="relative mx-auto flex flex-col z-0 items-center justify-center py-16 sm:py-20 lg:pb-28 transition-all animate-in lg:px-12 max-w-7xl">
+    <section className="relative mx-auto flex flex-col z-0 items-center justify-center py-16 sm:py-20 lg:pb-28 lg:px-12 max-w-7xl">
         
-        <div className="relative p-[1px] overflow-hidden rounded-full bg-gradient-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group">
+        <div className="relative p-[1px] overflow-hidden rounded-full bg-gradient-to-r from-rose-200 via-rose-500 to-rose-800 group">
             <Badge 
                 variant={'secondary'}
-                className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors duration-200"
+                className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50"
             >
-                <Sparkles className="!w-6 !h-6 mr-2 text-rose-600 animate-pulse"/>
+                <Sparkles className="!w-6 !h-6 mr-2 text-rose-600"/>
                 <p className="text-base text-rose-600">Powered by AI</p>
             </Badge>
         </div>
@@ -32,14 +32,14 @@ export default function HeroSection() {
         
         <Button 
             variant={'link'}
-            className="text-white mt-6 text-base sm:text-lg lg:text-xl rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 lg:mt-16 bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:no-underline font-bold shadow-lg transition-all duration-300 hover:scale-105 hover:transition-all"
+            className="text-white mt-6 text-base sm:text-lg lg:text-xl rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 lg:mt-16 bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:no-underline font-bold shadow-lg"
         >
             <a 
                 href="#pricing"
-                className="flex gap-2 items-center "
+                className="flex gap-2 items-center"
             >
                 <span>Try SumItUp</span>
-                <ArrowRight className="animate-pulse" />
+                <ArrowRight />
             </a>
         </Button>
 
